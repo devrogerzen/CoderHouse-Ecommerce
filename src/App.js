a038b9a0e3d63444c25bcc46bfc2cecabbc1c9dd
Roger Jimenez <devrogerzen@gmail.com>
2023-03-08 22:10:01 -0300

    clase 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import { NavBar } from "./NavBar";
 
 function App() {
   return (
+    <>
+    <NavBar />
     <div className="App">
       <h1>E-Commerce Para CoderHouse</h1>
     </div>
+    </>
   );
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,11 @@
+export const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Contact</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+};
